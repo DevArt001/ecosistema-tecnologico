1659748065ec294071bc928fb9e8c3350b3c5e93
Arturo <edilberto1992@gmail.com>
2026-05-20 19:30:34 +0000

    feat: S14 Swagger/OpenAPI funcionando en /api/docs/

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,7 @@ STATIC_URL = 'static/'
 CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
@@ -144,6 +145,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
